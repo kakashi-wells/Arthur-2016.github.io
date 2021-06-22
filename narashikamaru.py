a79b3a471ce9f94cd2b3403ee558734f0ce71fcb
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-22 00:30:15 -0500

    Problematico.

--- narashikamaru.py
+++ narashikamaru.py
@@ -1,8 +1,6 @@
 print("Ingresa 1 sombra")
 num1 = int(input())
 print("Ingresa 2 sombras")
-num2 = int(input())
-print("Ingresa 3 sombras")
-num3 = int(input())
-resultado = num1 + num2 + num3
+num2 = float(input())
+resultado = num1 + num2
 print ("El resultado es:" + str(resultado))
