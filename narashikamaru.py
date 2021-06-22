8d13574381fc273d83d5b104ab9fe59bda7781df
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-22 00:28:39 -0500

    Problematico, como me gustaria ser una nube.

---
+++ narashikamaru.py
@@ -0,0 +1,8 @@
+print("Ingresa 1 sombra")
+num1 = int(input())
+print("Ingresa 2 sombras")
+num2 = int(input())
+print("Ingresa 3 sombras")
+num3 = int(input())
+resultado = num1 + num2 + num3
+print ("El resultado es:" + str(resultado))
