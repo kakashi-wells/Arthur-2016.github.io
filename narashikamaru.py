d8213c8f80a40d270545e0a641208e4d5cd73e31
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-22 00:37:42 -0500

    Comer papas y ver las nubes, es lo maximo.

--- narashikamaru.py
+++ narashikamaru.py
@@ -1,6 +1,6 @@
 print("Ingresa 1 sombra")
 num1 = int(input())
-print("Ingresa 2 sombras")
-num2 = float(input())
-resultado = num1 + num2
+print("Ingresa 1 sombra/2 sombras")
+num2 = int(input())
+resultado = num1 + num2 
 print ("El resultado es:" + str(resultado))
